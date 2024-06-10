21e05e50cb66d6b1964c3ef1946ad9c01aa877fa
aveselov <aveselov@igatec.com>
2024-06-10 17:06:14 +0300

    classNames added

--- src/App.tsx
+++ src/App.tsx
@@ -8,12 +8,13 @@ import MainPage from "./pages/MainPage/MainPage"
 import { MainPageAsync } from "./pages/MainPage/MainPage.async"
 import { Theme, ThemeContext } from "./theme/ThemeContext"
 import { useTheme } from "./theme/useTheme"
+import { classNames } from "./helpers/classNames/classNames"
 
 const App = () =>  {
  const { theme, toggleTheme } = useTheme()
 
   return (
-    <div className={`app ${theme}`}>
+    <div className={classNames('app', {}, [theme])}>
       <button onClick={toggleTheme}>TOOGLE</button>
       <Link to={'/'}>Главная</Link>
       <Link to={'/about'}>О сайте</Link>        
