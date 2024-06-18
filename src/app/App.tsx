fe8a9cbd9a209bb35211c433ade01f30b0be9946
aveselov <aveselov@igatec.com>
2024-06-18 13:52:30 +0300

    locales added

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -8,18 +8,20 @@ import { MainPage } from "pages/MainPage"
 import { AppRouter } from "./providers/router"
 import { Navbar } from "widgets/Navbar"
 import { Sidebar } from "widgets/Sidebar"
+import { useTranslation } from "react-i18next"
 
 const App = () =>  {
  const { theme } = useTheme()
 
   return (
     <div className={classNames('app', {}, [theme])}>
-      
+        <Suspense fallback=''>
         <Navbar />
         <div className="content-page">
           <Sidebar />
           <AppRouter />
         </div>
+        </Suspense>
     </div>
   )
 }
