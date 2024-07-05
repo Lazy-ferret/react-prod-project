0c7ddcb32e4bc786672aff5e66feec485ef2a7ba
aveselov <aveselov@igatec.com>
2024-07-05 19:58:37 +0300

    ErrorBoundary added

---
+++ src/app/providers/ErrorBoundary/ui/BugButton.tsx
@@ -0,0 +1,24 @@
+import { useEffect, useState } from 'react';
+import { useTranslation } from 'react-i18next';
+import { Button } from 'shared/ui/Button/Button';
+
+// Компонент для тестирования ErrorBoundary
+export const BugButton = () => {
+    const [error, setError] = useState(false);
+    const { t } = useTranslation();
+
+    const onThrow = () => setError(true);
+
+    useEffect(() => {
+        if (error) {
+            throw new Error();
+        }
+    }, [error]);
+
+    return (
+        // eslint-disable-next-line i18next/no-literal-string
+        <Button onClick={onThrow}>
+            {t('throw error')}
+        </Button>
+    );
+};
