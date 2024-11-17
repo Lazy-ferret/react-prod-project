2e2bd979ca7c00136dd8eb2b1e86b1cc4783f48c
lazy_ferret <a.veselov83@gmail.com>
2024-11-17 22:04:51 +0300

    #45 refresh plugin added

--- src/pages/MainPage/ui/MainPage.tsx
+++ src/pages/MainPage/ui/MainPage.tsx
@@ -1,17 +1,10 @@
-import React, { useState } from 'react';
+import React from 'react';
 import { useTranslation } from 'react-i18next';
 import { Counter } from 'entities/Counter';
-import { Input } from 'shared/ui/Input/Input';
 import { BugButton } from '../../../app/providers/ErrorBoundary';
 
 function MainPage() {
     const { t } = useTranslation('main');
-
-    const [value, setValue] = useState('');
-
-    const onChange = (val: string) => {
-        setValue(val);
-    };
 
     return (
         <div>
