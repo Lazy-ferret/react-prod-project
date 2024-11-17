53abee2040db38fccdb8f01c39f830f2d7ca7871
lazy_ferret <a.veselov83@gmail.com>
2024-11-17 20:52:37 +0300

    #43 tests for profile added

--- src/widgets/Sidebar/ui/Sidebar/Sidebar.stories.tsx
+++ src/widgets/Sidebar/ui/Sidebar/Sidebar.stories.tsx
@@ -4,6 +4,7 @@ import { ComponentStory, ComponentMeta } from '@storybook/react';
 import 'app/styles/index.scss';
 import { ThemeDecorator } from 'shared/config/storybook/ThemeDecorator/ThemeDecorator';
 import { Theme } from 'app/providers/ThemeProvider';
+import { StoreDecorator } from 'shared/config/storybook/StoreDecorator/StoreDecorator';
 import { Sidebar } from './Sidebar';
 
 export default {
@@ -18,13 +19,25 @@ const Template: ComponentStory<typeof Sidebar> = (args) => <Sidebar {...args} />
 
 export const Light = Template.bind({});
 Light.args = {};
+Light.decorators = [
+    StoreDecorator({
+        user: { authData: {} },
+    }),
+];
 
 export const Dark = Template.bind({});
 Dark.args = {};
 Dark.decorators = [
     ThemeDecorator(Theme.DARK),
-    StoreDecorator()
+    StoreDecorator({
+        user: { authData: {} },
+    }),
 ];
 
 export const NoAutn = Template.bind({});
 NoAutn.args = {};
+NoAutn.decorators = [
+    StoreDecorator({
+        user: { },
+    }),
+];
