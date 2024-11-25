2d3296bb2b4b3bedb507414ba93d09ed7d7a860e
lazy_ferret <a.veselov83@gmail.com>
2024-11-25 22:06:41 +0300

    #48 wip

---
+++ src/pages/ArticleDetailsPage/ui/ArticleDetailsPage/ArticleDetailsPage.stories.ts
@@ -0,0 +1,24 @@
+import React, { FC } from 'react';
+import { ComponentStory, ComponentMeta } from '@storybook/react';
+
+import 'app/styles/index.scss';
+import { ThemeDecorator } from 'shared/config/storybook/ThemeDecorator/ThemeDecorator';
+import { Theme } from 'app/providers/ThemeProvider';
+import ArticleDetailsPage from './ArticleDetailsPage';
+
+export default {
+    title: 'pages/ArticleDetailsPage',
+    component: ArticleDetailsPage,
+    argTypes: {
+        backgroundColor: { control: 'color' },
+    },
+} as ComponentMeta<typeof ArticleDetailsPage>;
+
+const Template: ComponentStory<typeof ArticleDetailsPage> = () => <ArticleDetailsPage />;
+
+export const Normal = Template.bind({});
+Normal.args = {};
+
+export const Dark = Template.bind({});
+Dark.args = {};
+Dark.decorators = [ThemeDecorator(Theme.DARK)];
---
+++ src/pages/ArticlesPage/ui/ArticlesPage/ArticlesPage.stories.ts
@@ -0,0 +1,24 @@
+import React, { FC } from 'react';
+import { ComponentStory, ComponentMeta } from '@storybook/react';
+
+import 'app/styles/index.scss';
+import { ThemeDecorator } from 'shared/config/storybook/ThemeDecorator/ThemeDecorator';
+import { Theme } from 'app/providers/ThemeProvider';
+import ArticlesPage from './ArticlesPage';
+
+export default {
+    title: 'pages/ArticlesPage',
+    component: ArticlesPage,
+    argTypes: {
+        backgroundColor: { control: 'color' },
+    },
+} as ComponentMeta<typeof ArticlesPage>;
+
+const Template: ComponentStory<typeof ArticlesPage> = () => <ArticlesPage />;
+
+export const Normal = Template.bind({});
+Normal.args = {};
+
+export const Dark = Template.bind({});
+Dark.args = {};
+Dark.decorators = [ThemeDecorator(Theme.DARK)];
